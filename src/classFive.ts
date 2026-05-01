d3723d3a4b569c4299a7f603fc35c08dc51bf515
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-02 01:00:39 +0600

    class five

---
+++ src/classFive.ts
@@ -0,0 +1,12 @@
+let bazarList: string[] = ['eggs', 'milk', 'sugar']
+// bazarList.push(33)
+// let mixedArray = ['eggs', 12, 'milk', 2, 'sugar', 3]
+let mixedArray: (string | number) [] = ['eggs', 12, 'milk', 2, 'sugar', 3]
+mixedArray.push(33)
+
+// tuple
+let coordinates: [number,number] = [22,33]
+let couple: [string, string] = ['husband', 'wife']
+
+let nameAndRoll: [string, number] = ['mizba', 33]
+let xy: [string,string,number] = ['bangladesh', 'dhaka', 100]
